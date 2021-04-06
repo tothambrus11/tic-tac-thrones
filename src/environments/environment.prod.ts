16100e526c0f438d3b0345fdebf8b51453e1f463
Tóth Ambrus <ambrus@johetajava.hu>
2021-04-06 16:28:02 +0200

    idk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: "https://tictac-thrones.web.app/"
+  baseUrl: "https://tic-tac-thrones.tk/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: "https://tictac-thrones.web.app/"
+  baseUrl: "https://tic-tac-thrones.tk/"
 };
 
 /*
